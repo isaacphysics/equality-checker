9b4a932eeb7e8085cfbff9fe3717b3426a98f709
James Sharkey <jsharkey13@users.noreply.github.com>
2015-12-16 16:47:14 +0000

    Implement correct working version (== doesn't behave as expected) and allow implicit multiplication.

--- server/api.py
+++ server/api.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-from sympy.parsing.sympy_parser import parse_expr
+from sympy.parsing.sympy_parser import parse_expr, implicit_multiplication, standard_transformations
 from sympy.printing.latex import latex
+import sympy
 
 @app.route('/check', methods=["POST"])
 def check():
 	body = request.get_json(force=True)
 
-	target_expr = parse_expr(body["target"])
-	test_expr = parse_expr(body["test"])
+	target_expr = parse_expr(body["target"],transformations=standard_transformations+(implicit_multiplication,))
+	test_expr = parse_expr(body["test"],transformations=standard_transformations+(implicit_multiplication,))
 
 	print "Parsed target: %s\nParsed to check: %s" % (target_expr, test_expr)
 	
@@ -18,7 +19,7 @@ def check():
 		test=body["test"],
 		parsedTarget=latex(target_expr),
 		parsedTest=latex(test_expr),
-		equal=target_expr == test_expr
+		equal=sympy.simplify(test_expr - target_expr)==0
 	)
 
 if __name__ == '__main__':
