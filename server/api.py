07ad5d604423b4531a68fa685b417fef113ebd65
James Sharkey <jsharkey13@users.noreply.github.com>
2018-01-29 15:46:02 +0000

    Bump copyright years

--- server/api.py
+++ server/api.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 James Sharkey
+# Copyright 2018 James Sharkey
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- server/parsing.py
+++ server/parsing.py
@@ -1,4 +1,4 @@
-# Copyright 2017 James Sharkey
+# Copyright 2018 James Sharkey
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
