0ea2d77b2eea0b225e44cafb730e6e9b6830ef45
James Sharkey <jsharkey13@users.noreply.github.com>
2025-01-23 17:11:53 +0000

    Use multiple worker threads for parallel requests
    Also update the log format to add worker process ID and remove the
    HTTP Basic Auth username we never set.

--- checker/server/gunicorn_conf.py
+++ checker/server/gunicorn_conf.py
@@ -1,6 +1,6 @@
 bind = "0.0.0.0:5000"
 accesslog = '-'
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s - %(L)ss'
-workers = 1
+access_log_format = '%(p)s %(h)s %(l)s %(t)s "%(r)s" %(s)s - %(L)ss'
+workers = 4
 backlog = 30
 preload_app = True
